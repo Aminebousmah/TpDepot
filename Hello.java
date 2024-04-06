a1b9d122f9dafe4f8068a1038c73ad39959ca8fe
Aminebousmah <amine91200@live.fr>
2024-04-06 17:19:20 +0200

    Vive le devops

---
+++ Hello.java
@@ -0,0 +1,17 @@
+public class Exemple {
+
+    
+    public static void main(String[] args) {
+        
+        Exemple exemple = new Exemple();
+        
+        
+        exemple.afficherMessage();
+    }
+    
+    
+    public void afficherMessage() {
+        
+        System.out.println("Bonjour");
+    }
+}
